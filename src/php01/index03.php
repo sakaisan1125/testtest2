0783b0cf6fef7b3a2f036591d6b10f1bb7e6bb40
坂井　理 <sakaisan0908@gmail.com>
2025-07-02 00:22:04 +0900

    リモートリポジトリの変更

---
+++ src/php01/index03.php
@@ -0,0 +1,12 @@
+<?php
+$people=[
+    'person1'=>'taro',
+    'person2'=>'jiro',
+    'person3'=>'saburo'
+];
+
+var_dump($people);
+
+
+
+?>
---
+++ src/php01/index04.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+            var_dump(1!=2);
+            print "<br/>";
+            var_dump(2>=1);
+            print "<br/>";
+            var_dump(1!=1);
+
+         ?>
+
+
+
+
+
+</body>
+</html>
